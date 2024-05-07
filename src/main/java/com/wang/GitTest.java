1deb909ab91c39f9dd949c8df3d7c47ef6077568
AWoof <162292017+WangKesheng@users.noreply.github.com>
2024-05-07 15:38:54 +0800

    Update GitTest.java
    pull test commit

--- src/main/java/com/wang/GitTest.java
+++ src/main/java/com/wang/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git 333");
         System.out.println("hello git666");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
